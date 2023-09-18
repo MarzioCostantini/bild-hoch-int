270f35e0cad360923275b2e0fc7fc557bfd19468
Marzio Costantini <costantinimarzio@gmail.com>
2023-09-18 14:54:29 +0200

    add clodinary img

--- app/[locale]/components/HochzeitsfotografStarnberg.js
+++ app/[locale]/components/HochzeitsfotografStarnberg.js
@@ -10,7 +10,7 @@ const HochzeitsfotografStarnberg = () => {
       <article>
         <div>
           <Image
-            src="/img/home/Hochzeitsfotograf-in-München.avif"
+            src="https://res.cloudinary.com/dzlk6w9fd/image/upload/v1695041606/Pages/Home/Hochzeitsfotograf-in-Mu%CC%88nchen_byppvx.avif"
             height={200}
             width={270}
             alt="Hochzeitsfotograf Starnberg München Augsburg Garmisch-Partenkirchen"
@@ -34,15 +34,7 @@ const HochzeitsfotografStarnberg = () => {
         </div>
         <div className={styles.img02}>
           <Image
-            src="/img/home/Hochzeitsfotograf-München-33.avif"
-            height={250}
-            width={200}
-            alt="Hochzeitsfotograf Starnberg München Augsburg Garmisch-Partenkirchen"
-            style={{ width: "50%", height: "auto" }}
-            sizes="100vw"
-          />
-          <Image
-            src="/img/home/Hochzeitsfotograf-Starnberg-deko3-min.avif"
+            src="https://res.cloudinary.com/dzlk6w9fd/image/upload/v1695041595/Pages/Home/Hochzeitsfotograf_Mu%CC%88nchen_33_ttxio3.jpg"
             height={250}
             width={200}
             alt="Hochzeitsfotograf Starnberg München Augsburg Garmisch-Partenkirchen"
