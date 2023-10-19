a01086abc718a1620f65f923f0c492ca04546805
Marzio Costantini <costantinimarzio@gmail.com>
2023-10-19 17:11:13 +0200

    typ img Comp

--- app/[locale]/components/Unterseiten/HeroTwoImg.js
+++ app/[locale]/components/Unterseiten/HeroTwoImg.js
@@ -30,6 +30,8 @@ const HeroTwoImg = ({ title, subTitle, text, imgLinks, imgRechts }) => {
         width={290}
         alt="Hochzeitsfotograf Starnberg München Augsburg Garmisch-Partenkirchen"
       />
+      <img src={imgRechts} alt="" />
+      <h2>Haloo</h2>
     </section>
   );
 };
