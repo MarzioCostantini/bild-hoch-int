79dc859027f804f79d7b29565c7031a3d300b3ec
Marzio Costantini <costantinimarzio@gmail.com>
2023-10-24 15:07:46 +0200

    layout.jsx meta auskommentiert

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -11,27 +11,27 @@ import CookieBanner from "../../components/CookieBanner";
 
 // ! === META ===
 
-export const metadata = {
-  title: "Euer Hochzeitsfotograf in München | Authentisch und Echt ☀",
-  description:
-    "Ich bin Marzio Hochzeitsfotograf in München. Hochzeitsreportagen in München und Umgebung | Klickt für weitere Informationen einfach hier!",
-  openGraph: {
-    title: "Euer Hochzeitsfotograf in München | Authentisch und Echt ☀",
-    description:
-      "Ich bin Marzio Hochzeitsfotograf in München. Hochzeitsreportagen in München und Umgebung | Klickt für weitere Informationen einfach hier!",
-    url: "https://bild-hochzeit.de/",
-    siteName: "Euer Hochzeitsfotograf in München | Authentisch und Echt ",
-    type: "website",
-    images: [
-      {
-        url: "https://res.cloudinary.com/dzlk6w9fd/image/upload/v1689686554/Pages/Home/Titelbild_Hochzeitsfotograf_kgk1w7.jpg",
-        width: 800,
-        height: 600,
-      },
-    ],
-    type: "website",
-  },
-};
+// export const metadata = {
+//   title: "Euer Hochzeitsfotograf in München | Authentisch und Echt ☀",
+//   description:
+//     "Ich bin Marzio Hochzeitsfotograf in München. Hochzeitsreportagen in München und Umgebung | Klickt für weitere Informationen einfach hier!",
+//   openGraph: {
+//     title: "Euer Hochzeitsfotograf in München | Authentisch und Echt ☀",
+//     description:
+//       "Ich bin Marzio Hochzeitsfotograf in München. Hochzeitsreportagen in München und Umgebung | Klickt für weitere Informationen einfach hier!",
+//     url: "https://bild-hochzeit.de/",
+//     siteName: "Euer Hochzeitsfotograf in München | Authentisch und Echt ",
+//     type: "website",
+//     images: [
+//       {
+//         url: "https://res.cloudinary.com/dzlk6w9fd/image/upload/v1689686554/Pages/Home/Titelbild_Hochzeitsfotograf_kgk1w7.jpg",
+//         width: 800,
+//         height: 600,
+//       },
+//     ],
+//     type: "website",
+//   },
+// };
 
 export default function RootLayout({ children, params }) {
   const locale = useLocale();
